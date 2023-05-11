197c0946db840a2f71dce20c7e4964dcea78fe23
Anil K Sahu <aksahu095@gmail.com>
2023-05-11 07:17:47 +0000

    First commit in feature branch

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ class Hello
 {
 Public static void main(String args[])
 {
-System.out.println(“hello after author update”);
+System.out.println(“hello after author update in master branch”);
+System.out.println(“First update in feature branch”);
 }
 }
