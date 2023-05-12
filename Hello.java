22096d7a57b11537e6a9fb143e1f9dee13ea3b47
Anil K Sahu <aksahu095@gmail.com>
2023-05-12 15:23:54 +0000

    Edited from feature101

--- Hello.java
+++ Hello.java
@@ -2,7 +2,6 @@ class Hello
 {
 Public static void main(String args[])
 {
-System.out.println(“hello after author update in the master branch”);
-System.out.println(“First update in feature branch”);
+System.out.println("We are in feature101")
 }
 }
