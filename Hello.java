86a0df8fd48271eccc9cbde7c33a0237551fc34d
aksahu095 <113297307+aksahu095@users.noreply.github.com>
2023-06-21 22:32:49 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,6 @@ class Hello
 Public static void main(String args[])
 {
 System.out.println(“hello after author update in master branch”);
-System.out.println(“First update in feature branch”);
-  
+System.out.println(“First update in feature branch”); 
 }
 }
